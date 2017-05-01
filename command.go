proxypoke/i3ipc

Return success for empty replies, fixes #3

command.go
@@ -40,6 +40,11 @@ func (self *IPCSocket) Command(action string) (success bool, err error) {
 		return
 	}
 
+	if len(cmd_reply) == 0 {
+		success = true
+		return
+	}
+
 	success = cmd_reply[0].Success
 	if cmd_reply[0].Error == "" {
 		err = nil

mdirkse/i3ipc-go

Conflicts:
	command.go

command.go
@@ -43,6 +43,11 @@ func (socket *IPCSocket) Command(action string) (success bool, err error) {
 		return
 	}
 
+	if len(cmdReply) == 0 {
+		success = true
+		return
+	}
+
 	success = cmdReply[0].Success
 	if cmdReply[0].Error == "" {
 		err = nil